4853e5211244086411e1a44d19703b8f146f8375
Abraham0912 <09120912ahn@gmail.com>
2021-10-24 16:42:35 -0500

    create linea1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useState } from 'react';
 import listMemes from './jsons/listMeme.json';
 
 function App() {
+  const [linea1,setLinea1] = useState("");
+  const onChangeLinea1 = (e)=>{
+    setLinea1(e.target.value);
+  }
   return (
     <div>
       <select>
@@ -10,6 +14,8 @@ function App() {
           return <option key={item.id} value={item.id}>{item.name}</option>
         })}
       </select>
+      <br />
+      <input id="first_line"  value={linea1} onChange={onChangeLinea1} type="text" placeholder="Linea 1"/>
     </div>
   );
 }
